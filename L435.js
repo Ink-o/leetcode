f141624e8a53bc6abe11d4852fcd1a7de8a6e347
lemon <1970465772@qq.com>
2022-04-21 23:05:25 +0800

    feat:完善部分代码的注释

--- L435.js
+++ L435.js
@@ -29,3 +29,32 @@ var eraseOverlapIntervals = function(intervals) {
     return total;
 };
 console.log(eraseOverlapIntervals([ [1,2], [2,3], [3,4], [1,3] ]));
+
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ * 贪心解法
+ */
+var eraseOverlapIntervals = function(intervals) {
+    // 右边界从小到大排序
+    intervals.sort((a, b) => {
+        return a[1] - b[1];
+    })
+    // 这里的count初始化为1，因为一开始就收录了i = 0，并且此时的end还未初始化
+    let count = 1;
+    // 第一个末尾
+    let end = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+        let interval = intervals[i];
+        // 当前的最小值大于或等于重叠区间的最大值的时候，这时候就不需要删除，这时候继续更新end区间
+        if (interval[0] >= end) {
+            end = interval[1];
+            count += 1;
+        }
+    }
+    // 最后结果就是总长度-无需被删除的长度
+    return intervals.length - count;
+};
+console.log(eraseOverlapIntervals([ [1,2], [2,3], [3,4], [1,3] ]));
--- L56.js
+++ L56.js
@@ -3,25 +3,27 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if (intervals.length === 0) return intervals;
     let result = [];
     intervals.sort((a,b) => {
         return a[0] - b[0];
     });
-    for (let i = 0; i < intervals.length; i++) {
+    // 需要对比的对象
+    let prev = intervals[0]
+    for (let i = 1; i < intervals.length; i++) {
         const cur = intervals[i];
-        if (i == 0) {
-            result.push(cur);
-            continue;
-        }
-        let lastIndex = result.length - 1;
-        const resultLast = result[lastIndex];
-        if ((resultLast[1] >= cur[0])) {
-            result[lastIndex] = [resultLast[0], Math.max(resultLast[1], cur[1])]
+
+        // 区间不重叠，直接push进数组中
+        if (cur[0] > prev[1]) {
+            result.push(prev);
+            prev = cur;
         } else {
-            result.push(cur);
+            // 区间重叠，更新慢指针的最大值即可，因为最小值prev是保证小于等cur的最小值的
+            prev[1] = Math.max(prev[1], cur[1]);
         }
     }
+    // 最后需要把最后一个prev添加进数组中，因为这个prev是一定没有进队的
+    result.push(prev);
+
     return result;
 };
 console.log(merge([[1,3]]));
