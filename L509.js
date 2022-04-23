5bf75907b6fd6d4698bd47f7b3bdf9cce7181295
lemon <1970465772@qq.com>
2022-04-23 08:24:58 +0800

    feat:更新部分代码

--- L509.js
+++ L509.js
@@ -7,6 +7,7 @@ var fib = function(n) {
     let dp = [];
     dp[0] = 0;
     dp[1] = 1;
+    // 状态转移方程为dp[i - 1] + dp[i - 2]
     for (let i = 2; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
--- L746.js
+++ L746.js
@@ -3,9 +3,16 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    const dp = [cost[0], cost[1]];
+    // 初始化dp，默认第一第二步都需要消费
+    let dp = [cost[0], cost[1]];
+
+    // 遍历cost
     for (let i = 2; i < cost.length; i++) {
-        dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
+        // 确认递推公式，前两步的最小值+当前步花费值
+        dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
     }
+
+    // 取得最终结果
+    // 由于倒数第二步骤和倒数第一步是可以必达终点且已经把花费计算在内了，所以直接取两者的最小值即可
     return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
 };
