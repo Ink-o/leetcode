8162aa0860122526c7d2366b5ee96fb3bf89640f
lemon <1970465772@qq.com>
2022-04-23 23:31:00 +0800

    feat：更新部分动态规划代码

--- L509.js
+++ L509.js
@@ -1,6 +1,7 @@
 /**
  * @param {number} n
  * @return {number}
+ * On^2空间复杂度
  */
 // 动态规划解决斐波那契数列
 var fib = function(n) {
@@ -13,4 +14,27 @@ var fib = function(n) {
     }
     return dp[n];
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ * On空间复杂度（对dp进行压缩）
+ */
+// 动态规划解决斐波那契数列
+var fib = function(n) {
+    let dp = [];
+    dp[0] = 0;
+    dp[1] = 1;
+
+    // 状态转移方程为dp[i - 1] + dp[i - 2]
+    for (let i = 2; i <= n; i++) {
+        // 可以发现这个dp的状态转移方程仅需要用到前两个数值，所以仅仅使用两个数值即可，更新dp[1]和dp[0]即可
+        // 压缩空间复杂度
+        let tmp = dp[1];
+        dp[1] = d[1] - dp[0]
+        dp[0] = tmp;
+    }
+    return dp[1];
+};
+
 console.log(fib(4));
--- bag.js
+++ bag.js
@@ -5,22 +5,28 @@
  * @param {number} size 背包容量
  */
 function testWeightBagProblem(weight, value, size) {
-    const len = weight.length,
-        dp = new Array(len + 1).fill().map(
-            () => Array(size + 1).fill(0)
-        );
-    for (let i = 1; i <= len; i++) { // 物品
-        for (let j = 0; j <= size; j++) { // 背包剩余容量
-            if (j >= weight[i - 1]) {
-                dp[i][j] = Math.max(
-                    dp[i - 1][j],
-                    dp[i - 1][j - weight[i - 1]] + value[i - 1],
-                )
-            } else {
+    let len = weight.length;
+    // 定义dp[i][j] i为[0-i]个物品范围内，j为剩余背包容量。
+    // y轴代表的是物品编号，x轴代表的是背包剩余容量
+    // 表示从下标为[0-i]的物品里任意取，放进容量为j的背包，价值总和最大是多少。
+    let dp = new Array(len + 1).fill().map(_ => new Array(size + 1).fill(0));
+    
+    // 遍历物品，注意，物品编号需要从1开始，默认没有为0的物品编号，初始化值也是给了0
+    for (let i = 1; i <= len; i++) {
+        // 遍历背包容量
+        for (let j = 0; j <= size; j++) {
+            // 剩余背包容量不足以放入此物品，则i直接减1（意思是直接去1至i - 1的物品中查找合适的能放入进来的物品）
+            if (j < weight[i - 1]) {
                 dp[i][j] = dp[i - 1][j];
+                continue;
             }
+            // 剩余容量可以放下此物品，此时有两个选择，放或者不放，所以需要取其最大值
+            // 不放，缩小查找范围至i - 1即可，所剩背包容量也不需要改变。dp[i - 1][j]
+            // 放，缩小查找范围至i - 1，所剩背包容量需要减去当前物品的重量（j - weight[i - 1]），并且加上当前物品的价值
+            dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i - 1]] + value[i - 1]);
         }
     }
+    // 最终进行返回
     return dp[len][size];
 }
 
@@ -38,6 +44,6 @@ function testWeightBagProblem2(weight, value, size) {
 }
 
 function test () {
-    console.log(testWeightBagProblem2([1, 3, 4], [15, 20, 30], 4));
+    console.log(testWeightBagProblem([2, 3, 4, 5], [3, 4, 5, 8], 8));
 }
 test();
