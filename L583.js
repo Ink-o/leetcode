fa90b8a1f467c2659aa6d9fea8d43dd5cd0e8b75
ink <jiacheng.qiu@shopcider.com>
2022-05-18 10:13:20 +0800

    更新部分dp代码

---
+++ L583.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+ var minDistance = function(word1, word2) {
+    //  初始化dp数组，dp[i][j] 表示 word1 在 [0, i - 1] 和 word2 在 [0, j - 1] 范围里相同的最长子序列为 dp[i][j]
+    let dp = Array.from(Array(word1.length+1), () => Array(word2.length+1).fill(0));
+    // 求解word1和word2的最长相同子序列
+    for (let i = 1; i <= word1.length; i++) {
+        for (let j = 1; j <= word2.length; j++) {
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                 dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    let sameLength = dp[word1.length][word2.length];
+
+    // 结果为两个子串的长度相加 - 2倍最长相同子序列，这个就是需要删除的元素
+    return (word1.length + word2.length) - 2 * sameLength;
+};
