7578179e57bec11ad1994434f9523cd4fa54be96
Ink <jiacheng.qiu@shopcider.com>
2025-01-22 22:38:01 +0800

    feat: 更改

--- L100-150/L128.js
+++ L100-150/L128.js
@@ -1,12 +1,12 @@
 var longestConsecutive = function (nums) {
   let set = new Set(nums);
-  // 数字去重，如果有大量重复元素的话，将会变的非常慢
+  // 数字去重，如果有大量重复元素的话，将会变的非常慢（现在直接超时了）
   let arr = [...set]
   let ans = 0;
   for (let i = 0; i < arr.length; i++) {
     // 注意这里需要取准确值，目标值为arr
     let cur = arr[i];
-    // 只有当num - 1不存在时，才开始向后遍历num+1,num+2,num+3（确保此时是最小值）
+    // 只有当num - 1不存在时，才开始向后遍历num+1,num+2,num+3（确保此时是最小值，优化要做，不然没办法通过所有实例）
     if (!set.has(cur - 1)) {
       while (set.has(cur + 1)) {
         cur++;
